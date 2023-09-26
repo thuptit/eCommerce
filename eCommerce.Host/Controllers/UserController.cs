573df7fe0f2148d169e4c18489a5c6a989ce305b
thunv <thuitptit@gmail.com>
2023-09-26 21:55:30 +0700

    add file

---
+++ eCommerce.Host/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public async Task Get()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
