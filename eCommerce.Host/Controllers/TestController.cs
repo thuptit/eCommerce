be4e2458444982ed37f8ba4a59f416d228be49cb
thunv <thuitptit@gmail.com>
2023-09-28 22:01:03 +0700

    add test controller

---
+++ eCommerce.Host/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<string> Get()
+        {
+            await Task.CompletedTask;
+            return "Hello";
+        }
+    }
+}
