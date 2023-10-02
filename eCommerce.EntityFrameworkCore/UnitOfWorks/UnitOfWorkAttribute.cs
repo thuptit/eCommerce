b33ed1356d0e7d3c38b7bdf571246fd90dd2b2cc
thunv <thuitptit@gmail.com>
2023-10-02 23:00:42 +0700

    update unit of work attribute

--- eCommerce.EntityFrameworkCore/UnitOfWorks/UnitOfWorkAttribute.cs
+++ eCommerce.EntityFrameworkCore/UnitOfWorks/UnitOfWorkAttribute.cs
@@ -2,9 +2,12 @@ namespace eCommerce.EntityFrameworkCore.UnitOfWorks;
 
 public sealed class UnitOfWorkAttribute : Attribute
 {
-    public bool isTransactional { get; set; } = true;
-    
-    public UnitOfWorkAttribute(){}
+    public bool isTransactional { get; set; } = false;
+
+    public UnitOfWorkAttribute()
+    {
+        isTransactional = true;
+    }
     public UnitOfWorkAttribute(bool isTransactional)
     {
         this.isTransactional = isTransactional;
