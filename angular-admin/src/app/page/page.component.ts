1efc39e06d5123872a351273d2db060da937f42a
thunv <thuitptit@gmail.com>
2023-10-10 10:17:33 +0700

    fix selected menu item

--- angular-admin/src/app/page/page.component.ts
+++ angular-admin/src/app/page/page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NbMenuService } from '@nebular/theme';
 import { UserAuth } from 'src/core/models/user-auth.model';
 import { TokenAuthService } from 'src/core/services/token-auth.service';
 
@@ -9,13 +10,25 @@ import { TokenAuthService } from 'src/core/services/token-auth.service';
 })
 export class PageComponent implements OnInit {
   userInfo = {} as UserAuth;
-  constructor(private tokenAuth: TokenAuthService) {
+  constructor(private tokenAuth: TokenAuthService, private menuService: NbMenuService) {
   }
   ngOnInit(): void {
     this.getUserInfo();
+    this.menuService.onItemClick()
+      .subscribe(event => {
+        this.deselected();
+        event.item.selected = true;
+        return event;
+      });
   }
 
   getUserInfo() {
     this.userInfo = this.tokenAuth.getUser();
+  }
+
+  private deselected() {
+    this.menuService.getSelectedItem().subscribe(event => {
+      event.item.selected = false;
+    })
   }
 }
