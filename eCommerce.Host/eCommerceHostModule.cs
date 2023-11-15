b96e82989ef5cf1a4ef61bc547ae2af9730d7f67
thunv <thuitptit@gmail.com>
2023-11-15 14:29:11 +0700

    complete video call

--- eCommerce.Host/eCommerceHostModule.cs
+++ eCommerce.Host/eCommerceHostModule.cs
@@ -40,12 +40,10 @@ public class eCommerceHostModule : AbpModule
                     {
                         var accessToken = context.Request.Query["access_token"];
 
-                        // If the request is for our hub...
                         var path = context.HttpContext.Request.Path;
                         if (!string.IsNullOrEmpty(accessToken) &&
                             (path.StartsWithSegments("/signalr")))
                         {
-                            // Read the token out of the query string
                             context.Token = accessToken;
                         }
                         return Task.CompletedTask;
